c1d1c54f97e482819adcadd06d0dbab7ce6d9967
andrew <sbstn.andrew56@gmail.com>
2018-07-12 23:26:46 +0700

    make like for question and answer

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import './Home.css'
 import Avatar from '../../Component/Avatar/Avatar'
-import New from '../New/New'
+import New from '../../Component/New/New'
 class Home extends Component {
     constructor(props){
         super(props)
         this.state = {
-            data:[],
-            answer: ''
+            questionData:[],
+            answerData: [],
+            answer: '',
         }
     }
         async componentDidMount(){
         const RESPONSE = await fetch(`${process.env.REACT_APP_API_URL}/question`)
         const DATA = await RESPONSE.json()
-        console.log(DATA)
+        console.log('aaaaaa', DATA)
         this.setState({
-        data:DATA
+          questionData:DATA,
             }
         )
     }
@@ -28,7 +29,6 @@ class Home extends Component {
     }
 
     submitAnswer = (data) => {
-      console.log('data', data)
       axios({
         method: 'POST',
         url: `${process.env.REACT_APP_API_URL}/answer`,
@@ -43,31 +43,58 @@ class Home extends Component {
         window.location.reload();
     }
 
+    likeAnswer = (data) => {
+      axios({
+        method: 'POST',
+        url: `${process.env.REACT_APP_API_URL}/likeanswer`,
+        data: {
+          answerId: data
+        }
+      })
+        window.location.reload();
+    }
+
+    likeQuestion = (data) => {
+      axios({
+        method: 'POST',
+        url: `${process.env.REACT_APP_API_URL}/likequestion`,
+        data: {
+          questionId: data
+        }
+      })
+        window.location.reload();
+    }
+
     renderHome() {
-      if (!this.state.data) {
+      if (!this.state.questionData) {
         return null
       } else {
         return(
-          this.state.data.map(data => (
+          this.state.questionData.map(data => (
             <div key={data.id} className="home-body">
-                <div className="home-layout">
-                    <div className="home-avatar">
-                        <Avatar/> 
-                    </div>
-                    <div className="home-content">
-                        <div>
-                            <h2 className="title">Question</h2>
-                            <p className="title-content">{data.question}</p>
-                            <h2 className="title">Answer</h2>
-                            {data.answers.map(data => (
-                              <p key={data.id} className="location-content">{data.answer}</p>
-                            ))}
-                            <h2 className="title">Add New Answer</h2>
-                            <input value={this.state.answer} onChange={this.inputAnswer}/>
-                            <button onClick={() => this.submitAnswer(data)}>Submit</button>
-                        </div>                        
-                    </div>
+              <div className="home-layout">
+                <div className="home-avatar">
+                    <Avatar/> 
                 </div>
+                <div className="home-content">
+                  <div>
+                    <h2 className="title">Question</h2>
+                    <p className="title-content">{data.question}</p>
+                    <button>Like</button>
+                    <h2 className="title">Answer</h2>
+                    {data.answers.map(data => (
+                      <div>
+                        <p key={data.id} className="location-content">{data.answer}</p>
+                        <label>{data.like}</label>
+                        <button onClick={() => this.likeAnswer(data.id)}>like</button>
+                      </div>
+                    ))}
+                    <h2 className="title">Add New Answer</h2>
+                    <input value={this.state.answer} onChange={this.inputAnswer}/>
+                    <button onClick={() => this.submitAnswer(data)}>Submit</button>
+                  </div>                        
+                </div>
+              </div>
             </div>
             ))
         )
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Tab from './Pages/TabLogin/Tab';
 import Home from './Pages/Home/Home';
 import Profile from './Pages/Profile/Profile';
-import New from './Pages/New/New';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -32,7 +31,6 @@ class RouteList extends Component {
             <Route exact path="/" component={Tab} />
             <PrivateRoute exact path="/home" component={ Home } />
             <PrivateRoute path='/profile' component={Profile} />
-            <PrivateRoute path='/new-learning' component={New} />
           </div>
         </Router>
       </div>
